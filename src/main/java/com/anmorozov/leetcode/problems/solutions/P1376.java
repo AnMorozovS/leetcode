09de5dc76ee23e858487085a87240b73895e1d95
Andrey Morozov <github@anmorozov.com>
2023-06-03 16:44:23 +0300

    1376. Time Needed to Inform All Employees solution

--- src/main/java/com/anmorozov/leetcode/problems/solutions/P1376.java
+++ src/main/java/com/anmorozov/leetcode/problems/solutions/P1376.java
@@ -1,8 +1,60 @@
 package com.anmorozov.leetcode.problems.solutions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class P1376 {
 
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        return -1;
+
+        Map<Integer, List<Integer>> graphById = new HashMap<>();
+        for (int i = 0; i < manager.length; i++) {
+            if (manager[i] != -1) {
+                graphById.computeIfAbsent(manager[i], x -> new ArrayList<>()).add(i);
+            }
+        }
+        System.out.print(graphToLevelToString(graphById, headID));
+        return depthFirstSearchRecursive(headID, graphById, informTime);
+    }
+
+    private int depthFirstSearchRecursive(int cur, Map<Integer, List<Integer>> graph, int[] informTime) {
+        int maxTime = 0;
+        for (int employee : graph.getOrDefault(cur, new ArrayList<>())) {
+            int currentTime = depthFirstSearchRecursive(employee, graph, informTime);
+            if (currentTime > maxTime) {
+                maxTime = currentTime;
+            }
+        }
+        return maxTime + informTime[cur];
+    }
+
+    public void graphToLevel(Map<Integer, List<Integer>> graphById, int headID,
+                             Map<Integer, List<Integer>> graphByLevel, int level) {
+        graphByLevel.computeIfAbsent(level, x -> new ArrayList<>()).add(headID);
+        for (int employeeId : graphById.getOrDefault(headID, new ArrayList<>())) {
+            graphToLevel(graphById, employeeId, graphByLevel, level + 1);
+        }
+    }
+
+    public String graphToLevelToString(Map<Integer, List<Integer>> graphById, int headID) {
+        Map<Integer, List<Integer>> graphByLevel = new HashMap<>();
+        graphToLevel(graphById, headID, graphByLevel, 0);
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Integer, List<Integer>> entry : graphByLevel.entrySet()) {
+            builder.append(entry.getKey()).append(':').append('\t');
+            boolean flag = false;
+            for (int value : entry.getValue()) {
+                if (flag) {
+                    builder.append(", ");
+                } else {
+                    flag = true;
+                }
+                builder.append(value);
+            }
+            builder.append('\n');
+        }
+        return builder.toString();
     }
 }
