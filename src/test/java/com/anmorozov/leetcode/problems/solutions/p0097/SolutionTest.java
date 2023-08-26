07a9fedc7d755a09b1182f7cad9cacdba0f95e5c
AnMorozov <108075464+AnMorozovS@users.noreply.github.com>
2023-08-26 18:36:23 +0400

    97. Interleaving String (#130)
    Signed-off-by: Andrey Morozov <github@anmorozov.com>

--- src/test/java/com/anmorozov/leetcode/problems/solutions/p0097/SolutionTest.java
+++ src/test/java/com/anmorozov/leetcode/problems/solutions/p0097/SolutionTest.java
@@ -1,5 +1,4 @@
-package com.anmorozov.leetcode.problems.solutions.p2024;
-
+package com.anmorozov.leetcode.problems.solutions.p0097;
 
 import com.anmorozov.leetcode.common.BaseTest;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -13,23 +12,21 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("2024. Maximize the Confusion of an Exam.")
+@DisplayName("97. Interleaving String.")
 class SolutionTest {
 
-    static Stream<Arguments> maxConsecutiveAnswers() throws IOException {
-        BaseTest<SolutionTest, SolutionRecord> baseTest = new BaseTest<>(
-                SolutionTest.class, new TypeReference<>() {
+    static Stream<Arguments> isInterleave() throws IOException {
+        BaseTest<SolutionTest, SolutionRecord> baseTest = new BaseTest<>(SolutionTest.class, new TypeReference<>() {
         }, SolutionRecord::getArguments);
         return baseTest.prepare().stream();
     }
 
-
-    @DisplayName("Approach 1: Binary Search + Fixed Size Sliding Window")
-    @ParameterizedTest(name = "{0}: answerKey = {1}, k = {2}, output = {3}")
-    @MethodSource("maxConsecutiveAnswers")
-    void testApproach1(String ignoredMessage, String answerKey, int k, int output) {
+    @DisplayName("Approach 1 : simple")
+    @ParameterizedTest(name = "{0}, s1 = {1}, s2 = {2}, s3 = {3} output = {4}")
+    @MethodSource("isInterleave")
+    void testApproach1(String ignoredMessage, String s1, String s2, String s3, boolean output) {
         Solution solution = new Approach1();
-        int actual = solution.maxConsecutiveAnswers(answerKey, k);
+        boolean actual = solution.isInterleave(s1, s2, s3);
         assertEquals(output, actual, "Problem in " + solution.getClass().getSimpleName());
     }
 }
